66d2738bcb533b68591087c73b1ba62e162ba77b
ziad adel <ziadadel41@gmail.com>
2021-03-07 23:25:47 +0200

    ziad mange courses

--- Manage Coureses Form.Designer.cs
+++ Manage Coureses Form.Designer.cs
@@ -39,12 +39,12 @@ namespace Online_Exam_App
             this.label4 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.panel3 = new System.Windows.Forms.Panel();
             this.label3 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.panel2.SuspendLayout();
             this.panel5.SuspendLayout();
             this.panel4.SuspendLayout();
@@ -59,9 +59,10 @@ namespace Online_Exam_App
             this.panel2.Controls.Add(this.panel5);
             this.panel2.Controls.Add(this.panel4);
             this.panel2.Controls.Add(this.panel3);
-            this.panel2.Location = new System.Drawing.Point(704, 3);
+            this.panel2.Location = new System.Drawing.Point(528, 2);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(470, 685);
+            this.panel2.Size = new System.Drawing.Size(352, 557);
             this.panel2.TabIndex = 1;
             // 
             // panel5
@@ -72,9 +73,10 @@ namespace Online_Exam_App
             this.panel5.Controls.Add(this.button3);
             this.panel5.Controls.Add(this.Update);
             this.panel5.Controls.Add(this.button1);
-            this.panel5.Location = new System.Drawing.Point(3, 343);
+            this.panel5.Location = new System.Drawing.Point(2, 279);
+            this.panel5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(464, 339);
+            this.panel5.Size = new System.Drawing.Size(348, 275);
             this.panel5.TabIndex = 16;
             // 
             // button4
@@ -86,9 +88,10 @@ namespace Online_Exam_App
             this.button4.FlatAppearance.BorderSize = 0;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(185, 248);
+            this.button4.Location = new System.Drawing.Point(139, 202);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(227, 46);
+            this.button4.Size = new System.Drawing.Size(170, 37);
             this.button4.TabIndex = 3;
             this.button4.Text = "Back";
             this.button4.UseVisualStyleBackColor = false;
@@ -103,12 +106,14 @@ namespace Online_Exam_App
             this.button3.FlatAppearance.BorderSize = 0;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(185, 174);
+            this.button3.Location = new System.Drawing.Point(139, 141);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(227, 46);
+            this.button3.Size = new System.Drawing.Size(170, 37);
             this.button3.TabIndex = 2;
             this.button3.Text = "Delete";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Update
             // 
@@ -119,12 +124,14 @@ namespace Online_Exam_App
             this.Update.FlatAppearance.BorderSize = 0;
             this.Update.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Update.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Update.Location = new System.Drawing.Point(185, 100);
+            this.Update.Location = new System.Drawing.Point(139, 81);
+            this.Update.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Update.Name = "Update";
-            this.Update.Size = new System.Drawing.Size(227, 46);
+            this.Update.Size = new System.Drawing.Size(170, 37);
             this.Update.TabIndex = 1;
             this.Update.Text = "Update";
             this.Update.UseVisualStyleBackColor = false;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
             // 
             // button1
             // 
@@ -135,26 +142,29 @@ namespace Online_Exam_App
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(185, 26);
+            this.button1.Location = new System.Drawing.Point(139, 21);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(227, 46);
+            this.button1.Size = new System.Drawing.Size(170, 37);
             this.button1.TabIndex = 0;
             this.button1.Text = "Add";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // panel4
             // 
             this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel4.Controls.Add(this.comboBox2);
             this.panel4.Controls.Add(this.label4);
             this.panel4.Controls.Add(this.comboBox1);
             this.panel4.Controls.Add(this.label2);
-            this.panel4.Controls.Add(this.textBox2);
             this.panel4.Controls.Add(this.label1);
             this.panel4.Controls.Add(this.textBox1);
-            this.panel4.Location = new System.Drawing.Point(3, 129);
+            this.panel4.Location = new System.Drawing.Point(2, 105);
+            this.panel4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(464, 208);
+            this.panel4.Size = new System.Drawing.Size(348, 169);
             this.panel4.TabIndex = 15;
             // 
             // label4
@@ -162,9 +172,10 @@ namespace Online_Exam_App
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(16, 17);
+            this.label4.Location = new System.Drawing.Point(12, 14);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(120, 29);
+            this.label4.Size = new System.Drawing.Size(93, 24);
             this.label4.TabIndex = 18;
             this.label4.Text = "Course ID";
             // 
@@ -173,9 +184,10 @@ namespace Online_Exam_App
             this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.IntegralHeight = false;
-            this.comboBox1.Location = new System.Drawing.Point(153, 17);
+            this.comboBox1.Location = new System.Drawing.Point(115, 14);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(296, 33);
+            this.comboBox1.Size = new System.Drawing.Size(223, 28);
             this.comboBox1.TabIndex = 17;
             // 
             // label2
@@ -183,39 +195,33 @@ namespace Online_Exam_App
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(15, 132);
+            this.label2.Location = new System.Drawing.Point(11, 107);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(132, 32);
+            this.label2.Size = new System.Drawing.Size(102, 26);
             this.label2.TabIndex = 16;
             this.label2.Text = "Instructor";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(153, 129);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(296, 35);
-            this.textBox2.TabIndex = 15;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(15, 73);
+            this.label1.Location = new System.Drawing.Point(11, 59);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 32);
+            this.label1.Size = new System.Drawing.Size(82, 26);
             this.label1.TabIndex = 14;
             this.label1.Text = "Course";
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(153, 70);
+            this.textBox1.Location = new System.Drawing.Point(115, 57);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(296, 35);
+            this.textBox1.Size = new System.Drawing.Size(223, 29);
             this.textBox1.TabIndex = 13;
             // 
             // panel3
@@ -224,9 +230,10 @@ namespace Online_Exam_App
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel3.Controls.Add(this.label3);
-            this.panel3.Location = new System.Drawing.Point(3, 3);
+            this.panel3.Location = new System.Drawing.Point(2, 2);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(470, 123);
+            this.panel3.Size = new System.Drawing.Size(352, 100);
             this.panel3.TabIndex = 14;
             // 
             // label3
@@ -238,9 +245,10 @@ namespace Online_Exam_App
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Tahoma", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(168)))), ((int)(((byte)(38)))));
-            this.label3.Location = new System.Drawing.Point(122, 44);
+            this.label3.Location = new System.Drawing.Point(92, 36);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(239, 40);
+            this.label3.Size = new System.Drawing.Size(198, 33);
             this.label3.TabIndex = 13;
             this.label3.Text = "Manage Course";
             // 
@@ -250,23 +258,38 @@ namespace Online_Exam_App
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(9, 10);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(678, 667);
+            this.dataGridView1.Size = new System.Drawing.Size(508, 542);
             this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.IntegralHeight = false;
+            this.comboBox2.Location = new System.Drawing.Point(117, 105);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(223, 28);
+            this.comboBox2.TabIndex = 19;
             // 
             // Manage_Coureses_Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(61)))), ((int)(((byte)(86)))));
-            this.ClientSize = new System.Drawing.Size(1175, 691);
+            this.ClientSize = new System.Drawing.Size(881, 561);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.panel2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Manage_Coureses_Form";
             this.Text = "Manage_Coureses_Form";
+            this.Load += new System.EventHandler(this.Manage_Coureses_Form_Load);
             this.panel2.ResumeLayout(false);
             this.panel5.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
@@ -286,7 +309,6 @@ namespace Online_Exam_App
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel4;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button4;
@@ -295,5 +317,6 @@ namespace Online_Exam_App
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
--- Manage Coureses Form.cs
+++ Manage Coureses Form.cs
@@ -13,6 +13,7 @@ namespace Online_Exam_App
     public partial class Manage_Coureses_Form : Form
     {
         private int managerID;
+        OnlineExam ent = new OnlineExam();
 
         public Manage_Coureses_Form(int id)
         {
@@ -25,6 +26,93 @@ namespace Online_Exam_App
             Manager_Home_Form home_Form = new Manager_Home_Form(managerID);
             this.Close();
             home_Form.Show();
+        }
+
+        private void Manage_Coureses_Form_Load(object sender, EventArgs e)
+        {
+            var courses = ent.Courses.Select(d => new { d.CrsId, d.CrsName }).ToList();
+            dataGridView1.DataSource = courses;
+            var instructors = ent.Instructors.Select(d => d).ToList();
+            foreach (var item in instructors)
+            {
+                comboBox2.Items.Add(item.InsName);
+            }
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.CurrentCell.ColumnIndex > 0)
+            {
+                this.dataGridView1.CurrentCell.Selected = false;
+            }
+            else
+            {
+                comboBox2.Text = "";
+                int crsID = int.Parse( dataGridView1.SelectedCells[0].Value.ToString());
+                var myCrs = ent.Courses.Where(d => d.CrsId == crsID).Select(d => d).First();
+                var crsIns = ent.Courses.Where(c=>c.CrsId==myCrs.CrsId).Select(c => c.Instructors).ToList();
+                comboBox1.Text = myCrs.CrsId.ToString();
+                textBox1.Text = myCrs.CrsName;
+                //comboBox2.Items.Clear();                
+                foreach (var item in crsIns)
+                {
+                    comboBox2.Text+=item.Select(d=>d.InsName).First();
+                }
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text != "" && comboBox2.Text != "" && textBox1.Text != "")
+            {
+                Course crs = new Course();
+                crs.CrsName = textBox1.Text;
+                var myins = ent.Instructors.Where(i => i.InsName == comboBox2.Text).Select(i => i).First();
+                crs.Instructors.Add(myins);
+                ent.Courses.Add(crs);
+                ent.SaveChanges();
+                MessageBox.Show("ADDED");
+                var courses = ent.Courses.Select(d => new { d.CrsId, d.CrsName }).ToList();
+                dataGridView1.DataSource = courses;
+            }
+            else
+            {
+                MessageBox.Show("PLEASE ENTER VALID DATA");
+            }
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text != "" && comboBox2.Text != "" && textBox1.Text != "")
+            {
+                Course myCrs = new Course();
+                myCrs = ent.Courses.Find(int.Parse(comboBox1.Text.ToString()));
+                var myins = ent.Instructors.Where(i => i.InsName == comboBox2.Text).Select(i => i).First();
+                myCrs.CrsName = textBox1.Text;
+                myCrs.Instructors.Add(myins);
+                ent.SaveChanges();
+                MessageBox.Show("UPDATED");
+                var courses = ent.Courses.Select(d => new { d.CrsId, d.CrsName }).ToList();
+                dataGridView1.DataSource = courses;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text != "" && comboBox2.Text != "" && textBox1.Text != "")
+            {
+                Course crs = new Course();
+                crs = ent.Courses.Find(int.Parse(comboBox1.Text.ToString()));
+                crs.Instructors.Clear();
+                ent.Courses.Remove(crs);
+                ent.SaveChanges();
+                MessageBox.Show("DELETED");
+                var courses = ent.Courses.Select(d => new { d.CrsId, d.CrsName }).ToList();
+                dataGridView1.DataSource = courses;
+
+            }
         }
     }
 }
